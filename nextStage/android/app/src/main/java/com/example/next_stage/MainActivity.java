8e18bf8273692bae6c4e82f6b35de8d1ea5d7c5a
eddy-ckq <72985038+eddy-ckq@users.noreply.github.com>
2022-09-17 14:07:34 +0800

    Flutter

---
+++ nextStage/android/app/src/main/java/com/example/next_stage/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.next_stage;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- nextstage/ios/Flutter/flutter_export_environment.sh
+++ nextstage/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/eckq/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/eckq/Documents/nextstage"
+export "FLUTTER_APPLICATION_PATH=/Users/eckq/Documents/SC2006/nextStage"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
